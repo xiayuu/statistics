62baaeaed1e872080b23171276a10a97e78bea59
xiayu <xiayu0401@gmail.com>
2019-07-16 18:01:13 +0800

    add

--- preprocess.py
+++ preprocess.py
@@ -7,6 +7,7 @@ with open(dest,  encoding='utf-8') as dest_f:
     data_iter = csv.reader(dest_f)
     data = [ data for data in data_iter ]
 
+
 for l in data:
     print('url:   %s' % l[0])
     print('title: %s' % l[1])
