b78b47f80a8ded87b76298b6e65d4d702559c440
xiayu <xiayu.program@foxmail.com>
2019-07-18 15:26:14 +0800

    debug

--- preprocess.py
+++ preprocess.py
@@ -35,9 +35,16 @@ def filter_stopword(w):
 taglist = ['eng', 'n', 'nz']
 
 
-res = [ w for w, f in pseg.cut(data[-2][2]) if filter_stopword(w) and filter_tag(f, taglist) ]
+keyword_dict = {}
 
-print(res)
+for l in data:
+    for w, f in pseg.cut(l[2]):
+        if filter_stopword(w) and filter_tag(f, taglist):
+            keyword_dict[w] = keyword_dict.get(w, default=0) + 1
 
 
+f = open('keyword.txt', 'w')
+for k, v in keyword_dict:
+    f.write("%s, %d\n" % (k, v))
 
+f.close()
--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@ pip install -r requirements.txt
 wget -q http://localhost:5000/results/dump/cnblog.csv
 tail cnblog.csv
 python3 preprocess.py
+
+tail keyword.txt
+wc -l keyword.txt
